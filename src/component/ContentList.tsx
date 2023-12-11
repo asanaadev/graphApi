37015d7a5178b3039167bf516fcab8c759abec56
user <user@MacBook-Pro.local>
2023-12-11 14:58:42 +0700

    ListOwerviewV05

--- src/component/ContentList.tsx
+++ src/component/ContentList.tsx
@@ -18,7 +18,7 @@ const ContentList = () => {
 		body: "",
 	});
 
-	const { loading: MainLoading, error: MainError, data: MainData } = useQuery(GET_ALL_REPOSITORY, {
+	const { loading: MainLoading, error: MainError, data } = useQuery(GET_ALL_REPOSITORY, {
 		client,
 		variables: {
 			userLogin: "Zhumabai00",
@@ -34,18 +34,18 @@ const ContentList = () => {
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 				<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
 					Get started by editing&nbsp;
-					<code className="font-mono font-bold">{MainData.user.name}</code>
+					<code className="font-mono font-bold">{data.user.name}</code>
 				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 					<a
 						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						href={MainData.user.url}
+						href={data.user.url}
 						target="_blank"
 						rel="noopener noreferrer"
 					>
 						By{' '}
 						<img
-							src={MainData.user.avatarUrl}
+							src={data.user.avatarUrl}
 							alt="avatar"
 							className="rounded-full"
 							width={100}
@@ -66,7 +66,7 @@ const ContentList = () => {
 			</div>
 
 			<div className="mb-32 mt-2 gap-2 grid text-center md:max-w-5xl lg:w-full md:mb-0 lg:grid-cols-4 lg:text-left">
-				{MainData.user.repositories.edges.map((item: any) => (
+				{data.user.repositories.edges.map((item: any) => (
 					<a
 						key={item.node.id}
 						href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
